410d59e26271d28b4541b3219015c13ccfe56250
José Vicente Neto <joseneto@digix.com.br>
2019-02-11 13:49:36 -0300

    Implementing authenticated API

--- app.js
+++ app.js
@@ -8,4 +8,11 @@ app.get('/', (req, res) => {
     res.render("index.jade", { layout: false });
 });
 
+app.get('/account/authenticated', (req, res) => {
+    if (req.session.loggedIn)
+        res.sendStatus(200)
+    else
+        res.sendStatus(401)
+});
+
 app.listen(8080);
